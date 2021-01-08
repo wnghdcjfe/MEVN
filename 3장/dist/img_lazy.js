bd9201574cecfbc6a22020566fde11be1f012b6f
juhongchul <jhc9639@naver.com>
2021-01-09 07:10:15 +0900

    BE : 3장 코드 수정

--- 3장/dist/img_lazy.js
+++ 3장/dist/img_lazy.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() { 
-    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));   
-    let active = false;   
+    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"))
+    let active = false
     const lazyLoad = function() {   
       if (active === false) {
-        active = true; 
+        active = true 
         setTimeout(()=> { 
           lazyImages = lazyImages.map((lazyImage) => {   
             if (lazyImage.getBoundingClientRect().top <= window.innerHeight && window.getComputedStyle(lazyImage).display !== "none") {   
-              lazyImage.src = lazyImage.dataset.src;   
-              lazyImage.classList.remove("lazy");    
-              return null;
-            }else return lazyImage;
-          }).filter(image => image);
+              lazyImage.src = lazyImage.dataset.src
+              lazyImage.classList.remove("lazy")  
+              return null
+            }else return lazyImage
+          }).filter(image => image)
           if (!lazyImages.length) {
-            document.removeEventListener("scroll", lazyLoad); 
-          }else active = false;
-        }, 200);
+            document.removeEventListener("scroll", lazyLoad)
+          }else active = false
+        }, 200)
       }
-    }; 
-    document.addEventListener("scroll", lazyLoad); 
+    }
+    document.addEventListener("scroll", lazyLoad)
   });
--- 3장/server.js
+++ 3장/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const path = require('path')
-const app = express();
-const _path = path.join(__dirname, './dist') 
+import express from 'express' 
+import path from 'path'
+
+const PORT = 12010
+const app = express()
+const __dirname = path.resolve()
+const _path = path.join(__dirname, './dist')
 app.use('/', express.static(_path)) 
 app.listen(12010, ()=> {
-  console.log('lazy 이미지서버 : 12010시작 http://127.0.0.1:12010');
-});
+  console.log('lazy 이미지서버가 구동됩니다. port : http://127.0.0.1:1${PORT}')
+})
