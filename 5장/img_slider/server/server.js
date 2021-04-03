9c74cad0e8a6ec461169c44dcd955e588fa26579
juhongchul <jhc9639@naver.com>
2021-04-03 13:06:08 +0900

    b

--- 5장/img_slider/server/server.js
+++ 5장/img_slider/server/server.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
 const app = express()
 const img_path = path.join(__dirname, './img')
 const PORT = 12010
 const main = async () => { 
   app.use('/img', express.static(img_path))
-  // app.use(cors())
   app.listen(PORT, () => console.log(`이미지제공 서버가 시작됩니다. http://127.0.0.1:${PORT}`))
 }
 main()
